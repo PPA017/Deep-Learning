6bc164d60659a739a6d9a7882230956a81c3140b
Puscas Mihai <puscas.mihai2064@gmail.com>
2025-11-19 21:37:10 +0200

    Ex2 in progress

--- Lab01/Laboratory 01/Ex2.py
+++ Lab01/Laboratory 01/Ex2.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
-
+import numpy as np
 class LinearRegressionModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -15,6 +15,61 @@ torch.manual_seed(42)
 
 model_1 = LinearRegressionModel()
 
-print(f"The model's state dict(): {model_1.state_dict()}")
+#print(f"The model's state dict(): {model_1.state_dict()}")
  
+#===================================== EX 3 ==================================
+weight = 0.3
+bias = 0.9
+
+start = 0
+end = 1
+step = 0.01
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+train_split = int(0.8 * len(y))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+
+loss_fn = nn.L1Loss()
+
+optimizer = torch.optim.SGD(params=model_1.parameters(), lr=0.01)
+
+torch.manual_seed(42)
+
+epochs = 200
+
+epoch_count = []
+loss_values = []
+test_loss_values = []
+
+for epoch in range(epochs):
+    model_1.train()
+    
+    y_pred = model_1(X_train)
+    
+    loss = loss_fn(y_pred, y_train)
+    
+    optimizer.zero_grad()
+    
+    loss.backward()
+    
+    optimizer.step()
+    
+    model_1.eval()
+    
+    with torch.inference_mode():
+        test_pred = model_1(X_test)
+        
+        test_loss = loss_fn(test_pred, y_test)
+    
+    if epoch % 10 == 0:
+        epoch_count.append(epoch)
+        loss_values.append(loss)
+        test_loss_values.append(test_loss)
+        print(f"Epoch: {epoch} | Loss: {loss} | Test loss: {test_loss}")
+    
+np.array(torch.tensor(loss_values).numpy()), torch.tensor(test_loss_values).numpy()
+
         
